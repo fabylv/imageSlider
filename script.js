6fc9f154965a7a89b2671715ef2e58875145d0d8
Faby <fabylv@gmail.com>
2022-01-17 10:21:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const buttons = document.querySelectorAll('.btn');
 
 buttons.forEach((button) => {
 	button.addEventListener('click', () => {
-		const offset = button.classList.contains('next') ? 1 : -1;
+		const offset = button.classList.contains('btn-next') ? 1 : -1;
 		const slides = button.closest('.slider').querySelector('.slide-container');
 
 		const activeSlide = slides.querySelector('.active');
